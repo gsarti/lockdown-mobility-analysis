850f460d7be7626a87533f7cd39b7c7c2abe75e2
enricofallacara <enrico.fallacara@gmail.com>
2020-06-18 18:55:01 +0200

    modified plots comparison of mobility

--- scripts/compare_mobility_variation.R
+++ scripts/compare_mobility_variation.R
@@ -8,18 +8,20 @@ mobility_post <- read.csv('data/2020-05-05.csv',sep=";")
 graph_pre <- create_graph_from_data(mobility_pre, metric="n")
 graph_mid <- create_graph_from_data(mobility_mid, metric="n")
 graph_post <- create_graph_from_data(mobility_post, metric="n")
+
 par(mfrow=c(1,3))
-plot_size <- function(graph){E(graph)$metric_value/15000}
-plot(graph_pre, vertex.size=5, edge.width=plot_size(graph_pre), edge.arrow.size=0.02)
-plot(graph_mid, vertex.size=5, edge.width=plot_size(graph_mid), edge.arrow.size=0.02)
-plot(graph_post, vertex.size=5, edge.width=plot_size(graph_post), edge.arrow.size=0.02)
+plot_size <- function(graph){(as.numeric(E(graph)$metric_value)/mean(as.numeric(E(graph)$metric_value)))/10000}
+
+plot(graph_pre, vertex.size=5, edge.width=plot_size(graph_pre),edge.lty=c("dotted"), edge.arrow.size=0.02)
+plot(graph_mid, vertex.size=5, edge.width=plot_size(graph_mid),edge.lty=c("dotted"),  edge.arrow.size=0.02)
+plot(graph_post, vertex.size=5, edge.width=plot_size(graph_post),edge.lty=c("dotted"),  edge.arrow.size=0.02)
 
 # ... but is significantly lower inter-provincially
 graph_pre <- create_graph_from_data(mobility_pre, metric="n", loops=F)
 graph_mid <- create_graph_from_data(mobility_mid, metric="n", loops=F)
 graph_post <- create_graph_from_data(mobility_post, metric="n", loops=F)
 par(mfrow=c(1,3))
-plot_size <- function(graph){E(graph)$metric_value/15000}
-plot(graph_pre, vertex.size=5, edge.width=plot_size(graph_pre), edge.arrow.size=0.02)
-plot(graph_mid, vertex.size=5, edge.width=plot_size(graph_mid), edge.arrow.size=0.02)
-plot(graph_post, vertex.size=5, edge.width=plot_size(graph_post), edge.arrow.size=0.02)
+
+plot(graph_pre, vertex.size=5, edge.width=plot_size(graph_pre),edge.lty=c("dotted"),  edge.arrow.size=0.02)
+plot(graph_mid, vertex.size=5, edge.width=plot_size(graph_mid),edge.lty=c("dotted"),  edge.arrow.size=0.02)
+plot(graph_post, vertex.size=5, edge.width=plot_size(graph_post),edge.lty=c("dotted"),  edge.arrow.size=0.02)
