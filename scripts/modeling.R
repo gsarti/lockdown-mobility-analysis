0e4781352a04444d9e141fd6f2a046c4351b3d56
enricofallacara <enrico.fallacara@gmail.com>
2020-06-25 13:17:03 +0200

    started the analysis

--- scripts/modeling.R
+++ scripts/modeling.R
@@ -1,5 +1,6 @@
 source('src/utils.R')
 source('src/plot.R')
+library(intergraph)
 
 mobility_pre <- read.csv('data/2020-02-25.csv',sep=";")
 mobility_mid <- read.csv('data/2020-03-10.csv',sep=";")
@@ -43,4 +44,60 @@ gc_pre$layout <- cbind(V(gc_pre)$x, V(gc_pre)$y)
 gc_mid$layout <- cbind(V(gc_mid)$x, V(gc_mid)$y)
 gc_post$layout <- cbind(V(gc_post)$x, V(gc_post)$y)
 
+# Modelling
+gc_pre_net <- intergraph::asNetwork(gc_pre)
+gc_mid_net <- intergraph::asNetwork(gc_mid)
+gc_post_net <- intergraph::asNetwork(gc_post)
 
+# First attempt with all vertex features and only edges
+gc_pre_model <- ergm(gc_pre_net ~ edges+ nodecov('covid_cases') + nodecov('covid_deaths') + 
+                       nodecov('population') + nodecov('surface') + nodecov('pop_density') + 
+                       nodecov('municipalities'))
+summary(gc_pre_model)
+
+gc_mid_model <- ergm(gc_mid_net ~ edges+ nodecov('covid_cases') + nodecov('covid_deaths') + 
+                       nodecov('population') + nodecov('surface') + nodecov('pop_density') + 
+                       nodecov('municipalities'))
+summary(gc_mid_model)
+
+gc_post_model <- ergm(gc_post_net ~ edges+ nodecov('covid_cases') + nodecov('covid_deaths') + 
+                       nodecov('population') + nodecov('surface') + nodecov('pop_density') + 
+                       nodecov('municipalities'))
+summary(gc_post_model)
+
+
+# Second attempt with only significant vertex features from previous analysis and only edges
+gc_pre_model <- ergm(gc_pre_net ~ edges + nodecov('covid_deaths') +  nodecov('population'))
+summary(gc_pre_model)
+
+gc_mid_model <- ergm(gc_mid_net ~ edges + nodecov('covid_deaths') + nodecov('population'))
+summary(gc_mid_model)
+
+gc_post_model <- ergm(gc_post_net ~ edges + nodecov('covid_deaths') + nodecov('population'))
+summary(gc_post_model)
+
+# Third attempt with only significant vertex features from previous analysis and some statistics of the graphs
+gc_pre_model <- ergm(gc_pre_net ~ edges + idegree1.5 + odegree1.5 + transitive + mutual
+                     + nodecov('covid_deaths') +  nodecov('population'))
+summary(gc_pre_model)
+mcmc.diagnostics(gc_pre_model)
+
+gc_mid_model <- ergm(gc_pre_net ~ edges + idegree1.5 + odegree1.5 + transitive + mutual
+                     + nodecov('covid_deaths') +  nodecov('population'))
+summary(gc_mid_model)
+mcmc.diagnostics(gc_mid_model)
+
+gc_post_model <- ergm(gc_pre_net ~ edges + idegree1.5 + odegree1.5 + transitive + mutual
+                      + nodecov('covid_deaths') +  nodecov('population'))
+summary(gc_post_model)
+mcmc.diagnostics(gc_post_model)
+
+??'ergm-terms'
+
+degree
+idegree
+idegree1.5
+odegree
+odegree1.5 
+transitive 
+mutual
